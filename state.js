55471564358829365fc3b372fabc2b47cf302598
Shlomis23 <selaaa@gmail.com>
2026-03-24 10:49:39 +0200

    Fix weight save 400 by sending required user_id

--- state.js
+++ state.js
@@ -38,6 +38,7 @@ let state = {
   sessions: [], workouts: loadWorkouts(), settings: loadSettings(),
   manageOpenId: null, editingNameId: null, selectedWorkoutId: null, expandedSessions: new Set(),
   weightLogs: [], weightGoal: { start_weight: null, goal_weight: null }, historyTab: "workouts",
+  userId: null,
   weightRangeDays: 7, weightRangeMode: "preset", weightCustomRange: { from: "", to: "" },
   workoutNote: "", monthViewYear: null, monthViewMonth: null, pendingGoal: null,
   editingExKey: null, exerciseLibrary: [], workoutExtras: [], dashboardAnimatedOnce: false,
